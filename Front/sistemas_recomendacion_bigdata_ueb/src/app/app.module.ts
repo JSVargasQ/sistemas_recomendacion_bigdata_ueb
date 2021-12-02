9202f4473451a218f35e7a8f34c82735b8cebd87
JSVargasQ <42383191+Sagrav10@users.noreply.github.com>
2021-12-01 21:10:04 -0500

    Add componetns and refactor structure of routes

--- Front/sistemas_recomendacion_bigdata_ueb/src/app/app.module.ts
+++ Front/sistemas_recomendacion_bigdata_ueb/src/app/app.module.ts
@@ -4,24 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BarChartComponent } from './components/bar-chart/bar-chart.component';
-import { LineChartComponent } from './components/line-chart/line-chart.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { NotificationDropdownComponent } from './components/notification-dropdown/notification-dropdown.component';
-import { UserDropdownComponent } from './components/user-dropdown/user-dropdown.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    BarChartComponent,
-    LineChartComponent,
-    NavbarComponent,
-    NotificationDropdownComponent,
-    UserDropdownComponent,
-    SidebarComponent
   ],
   imports: [
     BrowserModule,
